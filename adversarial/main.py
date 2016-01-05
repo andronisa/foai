b44a89c2bb0717b9ac850699fe9a95879ec02a13
Antonios Andronis <aa3e15@soton.ac.uk>
2016-01-05 13:56:44 +0000

    foobar last

--- adversarial/main.py
+++ adversarial/main.py
@@ -10,42 +10,25 @@ class MinMaxClass:
             'ab': 0
         }
 
-    def TicTacToe(self, X, O, width=3):
-        """Play a tic-tac-toe game between the two given functions. After each
-        turn, yield the new board.
-        Each function should get a tic-tac-toe board and a char - 'X' or 'O',
-        that represents the current turn, and return the number of
-        square where it wants to put a sign.
-        width is the board's width and length - it's 3 as default.
+    def Tic_Tac_Toe(self, X, O, game_width=3):
+        tile_board = (set(), set(), game_width)
 
-        X, O -- functions
-        width -- natural number
-        """
-
-        board = (set(), set(), width)
-
-        turn = 'X'
-        while self.result(board) is False:
-            if turn == 'X':
-                board[0].add(X(board, turn))
+        player = 'X'
+        while self.calculate(tile_board) is False:
+            if player == 'X':
+                tile_board[0].add(X(tile_board, player))
             else:
-                board[1].add(O(board, turn))
-            yield board
-            turn = list({'X', 'O'} - set([turn]))[0]
+                tile_board[1].add(O(tile_board, player))
+            yield tile_board
+            player = list({'X', 'O'} - set([player]))[0]
 
-    def display_tic_tac_toe(self, X, O, width=3):
-        """Play a tic-tac-toe game (see TicTacToe's docstring for explanation) and
-        display the new board to the user when a player plays, and the result of
-        the game after its end.
-
-        X, O - functions
-        width - natural number"""
-        for board in self.TicTacToe(X, O, width):
+    def display_tic_tac_toe(self, X, O, game_width=3):
+        for tile_board in self.Tic_Tac_Toe(X, O, game_width):
             # os.system('cls' if os.name == 'nt' else 'clear')  # clearscreen
             print("")
             print("")
-            print self.str_board(board)
-        winner = self.result(board)
+            print self.print_board(tile_board)
+        winner = self.calculate(tile_board)
         if winner in {'X', 'O'}:
             print winner + ' won!'
         elif winner == None:
@@ -54,106 +37,88 @@ class MinMaxClass:
             raise ValueError("The game didn't end")
         return winner, self.iterations['mm'], self.iterations['ab']
 
-    def result(self, board):
-        """Return 'X' if X won in the given board, 'O' if O won, None if the game
-        ended with a tie, False if the game didn't end yet, and raise an exception
-        if it looks like X and O won both (the board cannot be reached using a
-        legal game)."""
+    def calculate(self, tile_board):
+        x_squares, o_squares, game_width = tile_board
+        rrrs = [{game_width * rrr + col + 1 for col in range(game_width)} for rrr in range(game_width)]
+        cols = [{game_width * rrr + col + 1 for rrr in range(game_width)} for col in range(game_width)]
+        diagonals = [{game_width * i + i + 1 for i in range(game_width)},
+                     {game_width * i + game_width - i for i in range(game_width)}]
 
-        x_squares, o_squares, width = board
-        rows = [{width * row + col + 1 for col in range(width)} for row in range(width)]
-        cols = [{width * row + col + 1 for row in range(width)} for col in range(width)]
-        diagonals = [{width * i + i + 1 for i in range(width)},
-                     {width * i + width - i for i in range(width)}]
-
-        lines = rows + cols + diagonals
+        lines = rrrs + cols + diagonals
 
         x_won = any(line.issubset(x_squares) for line in lines)
         o_won = any(line.issubset(o_squares) for line in lines)
 
         if x_won:
             if o_won:
-                raise ValueError("Illegal board")
+                raise ValueError("Illegal tile_board")
             return 'X'
         if o_won:
             return 'O'
-        if x_squares | o_squares == set(range(1, width ** 2 + 1)):
-            # Nobody won, but the board is full
+        if x_squares | o_squares == set(range(1, game_width ** 2 + 1)):
+            # Nobody won, but the tile_board is full
             return None  # Tie
 
         return False
 
-    def str_board(self, board):
-        """Return the board in a string representation, to print it."""
+    def print_board(self, tile_board):
         return_str = ''
-        x_squares, o_squares, width = board
-        for row in range(width):
-            for col in range(width):
-                square = width * row + col + 1
+        x_squares, o_squares, game_width = tile_board
+        for rrr in range(game_width):
+            for col in range(game_width):
+                square = game_width * rrr + col + 1
                 return_str += 'X' if square in x_squares else 'O' if square in \
                                                                      o_squares else ' '
-                if col != width - 1: return_str += ' | '
-            if row != width - 1: return_str += '\n' + '--+-' * (width - 1) + '-\n'
+                if col != game_width - 1: return_str += ' | '
+            if rrr != game_width - 1: return_str += '\n' + '--+-' * (game_width - 1) + '-\n'
         return return_str
 
-    def human_player(self, board, turn):
-        """Display the board to the user and ask him where does he want to put a
-        sign. Return the square."""
-        x_squares, o_squares, width = board
+    def human_player(self, tile_board, player):
+
+        x_squares, o_squares, game_width = tile_board
         print("")
         print("")
-        print self.str_board(board)
+        print self.print_board(tile_board)
 
         while True:
             try:
                 square = int(
-                        raw_input('Where do you want to add ' + turn + '? '))
-                assert 0 < square <= width ** 2 and \
+                        raw_input('Where do you want to add ' + player + '? '))
+                assert 0 < square <= game_width ** 2 and \
                        square not in x_squares | o_squares
                 return square  # this will happen if there were no exceptions
             except:
-                print ('You should write an integer between 1 and ' + str(width ** 2) +
+                print ('You should write an integer between 1 and ' + str(game_width ** 2) +
                        ', that represents a blank square.')
 
-    def random_player(self, board, player):
-        """Return a square where it's worthwhile to play according to the minimax
-        algorithm."""
-        x_squares, o_squares, width = board
+    def random_player(self, tile_board, player):
+        x_squares, o_squares, game_width = tile_board
 
-        available_squares = list(set(range(1, width ** 2 + 1)) - (x_squares | o_squares))
+        available_squares = list(set(range(1, game_width ** 2 + 1)) - (x_squares | o_squares))
         random.shuffle(available_squares)
 
         return random.choice(available_squares)
 
-    def minimax_player(self, board, player):
-        """Return a square where it's worthwhile to play according to the minimax
-        algorithm."""
+    def minimax_player(self, tile_board, player):
+
         print("Total Iterations of minimax : " + str(self.iterations['mm']) + " - " + player)
-        return self.minimax_best_square(board, player)[0]
+        return self.minimax_best_square(tile_board, player)[0]
 
-    def minimax_score_board(self, board, turn):
-        """Return 1, 0 or -1 according to the minimax algorithm -- 1 if the player
-        that has the given turn has a winning strategy, 0 if he doesn't have a
-        winning strategy but he has a tie strategy, and -1 if he will lose anyway
-        (assuming his opponent is playing a perfect game)."""
-
-        if self.result(board) == turn:
+    def minimax_score_tile_board(self, tile_board, player):
+        if self.calculate(tile_board) == player:
             return 1
-        if self.result(board) is 'Tie':
+        if self.calculate(tile_board) is 'Tie':
             return 0
-        if self.result(board) is not False:
+        if self.calculate(tile_board) is not False:
             return -1
-        return self.minimax_best_square(board, turn)[1]
+        return self.minimax_best_square(tile_board, player)[1]
 
-    def minimax_best_square(self, board, player):
-        """Choose a square where it's worthwhile to play in the given board and
-        turn, and return a tuple of the square's number and it's score according
-        to the minimax algorithm."""
-        x_squares, o_squares, width = board
+    def minimax_best_square(self, tile_board, player):
+        x_squares, o_squares, game_width = tile_board
         max_score = -2
 
         opponent = list({'X', 'O'} - set([player]))[0]
-        available_squares = list(set(range(1, width ** 2 + 1)) - (x_squares | o_squares))
+        available_squares = list(set(range(1, game_width ** 2 + 1)) - (x_squares | o_squares))
         random.shuffle(available_squares)
 
         for square in available_squares:
@@ -162,9 +127,9 @@ class MinMaxClass:
             self.iterations['mm'] += 1
 
             # Iterate over the blank squares, to get the best square to play
-            new_board = (x_squares | set([square] if player == 'X' else []),) + \
-                        (o_squares | set([square] if player == 'O' else []), width)
-            score = -self.minimax_score_board(new_board, opponent)
+            new_tile_board = (x_squares | set([square] if player == 'X' else []),) + \
+                             (o_squares | set([square] if player == 'O' else []), game_width)
+            score = -self.minimax_score_tile_board(new_tile_board, opponent)
 
             if score == 1:
                 return square, 1
@@ -173,44 +138,37 @@ class MinMaxClass:
                 max_score, max_square = score, square
         return max_square, max_score
 
-    def alpha_beta_player(self, board, player, alpha=-2, beta=2):
-        """Return a square where it's worthwhile to play according to the minimax
-        algorithm."""
-
+    def alpha_beta_player(self, tile_board, player, alpha=-2, beta=2):
         print("Total Iterations of minimax : " + str(self.iterations['ab']) + " - " + player)
-        return self.alpha_beta_best_square(board, player, alpha, beta)[0]
+        return self.alpha_beta_best_square(tile_board, player, alpha, beta)[0]
 
-    def alpha_beta_score_board(self, board, player, alpha, beta):
-        """Return 1, 0 or -1 according to the minimax algorithm -- 1 if the player
-        that has the given turn has a winning strategy, 0 if he doesn't have a
-        winning strategy but he has a tie strategy, and -1 if he will lose anyway
-        (assuming his opponent is playing a perfect game)."""
+    def alpha_beta_score_tile_board(self, tile_board, player, alpha, beta):
 
-        if self.result(board) == player:
+        if self.calculate(tile_board) == player:
             return 1
-        if self.result(board) is None:
+        if self.calculate(tile_board) is None:
             return 0
-        if self.result(board) is not False:
+        if self.calculate(tile_board) is not False:
             return -1
-        return self.alpha_beta_best_square(board, player, alpha, beta)[1]
+        return self.alpha_beta_best_square(tile_board, player, alpha, beta)[1]
 
-    def alpha_beta_best_square(self, board, player, alpha, beta):
-        """Choose a square where it's worthwhile to play in the given board and
+    def alpha_beta_best_square(self, tile_board, player, alpha, beta):
+        """Choose a square where it's worthwhile to play in the given tile_board and
         turn, and return a tuple of the square's number and it's score according
         to the minimax algorithm."""
-        x_squares, o_squares, width = board
+        x_squares, o_squares, game_width = tile_board
 
         opponent = list({'X', 'O'} - set([player]))[0]
-        available_squares = list(set(range(1, width ** 2 + 1)) - (x_squares | o_squares))
+        available_squares = list(set(range(1, game_width ** 2 + 1)) - (x_squares | o_squares))
         random.shuffle(available_squares)
 
         for square in available_squares:
             self.iterations['ab'] += 1
 
             # Iterate over the blank squares, to get the best square to play
-            new_board = (x_squares | set([square] if player == 'X' else []),) + \
-                        (o_squares | set([square] if player == 'O' else []), width)
-            score = -self.alpha_beta_score_board(new_board, opponent, alpha, beta)
+            new_tile_board = (x_squares | set([square] if player == 'X' else []),) + \
+                             (o_squares | set([square] if player == 'O' else []), game_width)
+            score = -self.alpha_beta_score_tile_board(new_tile_board, opponent, alpha, beta)
 
             if player == 'O':
                 if score > alpha:
@@ -230,7 +188,7 @@ class MinMaxClass:
 
 
 def mm_vs_ab():
-    result = {
+    calculate = {
         'mm_iters': [],
         'ab_iters': [],
         'mm_wins': 0,
@@ -241,20 +199,20 @@ def mm_vs_ab():
     for i in range(100):
         solver = MinMaxClass(['mm', 'ab'])
         minmax_against_alphabeta = solver.display_tic_tac_toe(X=solver.minimax_player, O=solver.alpha_beta_player,
-                                                              width=3)
+                                                              game_width=3)
         if minmax_against_alphabeta[0] == 'X':
-            result['mm_wins'] += 1
+            calculate['mm_wins'] += 1
         elif minmax_against_alphabeta[0] == 'O':
-            result['ab_wins'] += 1
+            calculate['ab_wins'] += 1
         else:
-            result['ties'] += 1
-        result['mm_iters'].append(minmax_against_alphabeta[1])
-        result['ab_iters'].append(minmax_against_alphabeta[2])
-    return result
+            calculate['ties'] += 1
+        calculate['mm_iters'].append(minmax_against_alphabeta[1])
+        calculate['ab_iters'].append(minmax_against_alphabeta[2])
+    return calculate
 
 
 def ab_vs_mm():
-    result = {
+    calculate = {
         'mm_iters': [],
         'ab_iters': [],
         'mm_wins': 0,
@@ -265,37 +223,37 @@ def ab_vs_mm():
     for i in range(100):
         solver = MinMaxClass(['mm', 'ab'])
         minmax_against_alphabeta = solver.display_tic_tac_toe(X=solver.alpha_beta_player, O=solver.minimax_player,
-                                                              width=3)
+                                                              game_width=3)
         if minmax_against_alphabeta[0] == 'O':
-            result['mm_wins'] += 1
+            calculate['mm_wins'] += 1
         elif minmax_against_alphabeta[0] == 'X':
-            result['ab_wins'] += 1
+            calculate['ab_wins'] += 1
         else:
-            result['ties'] += 1
-        result['mm_iters'].append(minmax_against_alphabeta[1])
-        result['ab_iters'].append(minmax_against_alphabeta[2])
-    return result
+            calculate['ties'] += 1
+        calculate['mm_iters'].append(minmax_against_alphabeta[1])
+        calculate['ab_iters'].append(minmax_against_alphabeta[2])
+    return calculate
 
 
-def plot_wins(result, title):
+def plot_wins(calculate, title):
     OX = [
         'MinMax',
         'Alpha Beta',
         'Tie'
     ]
 
-    mm = result['mm_wins']
-    ab = result['ab_wins']
-    ties = result['ties']
+    mm = calculate['mm_wins']
+    ab = calculate['ab_wins']
+    ties = calculate['ties']
 
     OY = [mm, ab, ties]
 
     fig = plt.figure()
 
-    width = .20
+    game_width = .20
     ind = np.arange(len(OY))
     barlist = plt.bar(ind, OY, align='center')
-    plt.xticks(ind + width / 2, OX)
+    plt.xticks(ind + game_width / 2, OX)
 
     barlist[0].set_color('r')
     barlist[1].set_color('b')
@@ -309,13 +267,13 @@ def plot_wins(result, title):
     plt.show()
 
 
-def plot_lines(result, limit, title):
-    mm = result['mm_iters']
-    ab = result['ab_iters']
+def plot_lines(calculate, limit, title):
+    mm = calculate['mm_iters']
+    ab = calculate['ab_iters']
     mm_average = sum(mm) / len(mm)
     ab_average = sum(ab) / len(ab)
 
-    lines = plt.plot(mm, 'xr-', ab, 'xb-', linewidth=4)
+    lines = plt.plot(mm, 'xr-', ab, 'xb-', game_width=4)
 
     plt.legend((lines[0], lines[1]),
                ('MinMax average iterations: ' + str(mm_average), 'AlphaBeta average iterations: ' + str(ab_average)),
@@ -332,14 +290,14 @@ def plot_lines(result, limit, title):
 
 if __name__ == "__main__":
     solver = MinMaxClass(['mm', 'ab'])
-    # solver.display_tic_tac_toe(X=solver.human_player, O=solver.alpha_beta_player, width=3)
-    # result = solver.display_tic_tac_toe(X=solver.minimax_player, O=solver.alpha_beta_player, width=3)
-    result = mm_vs_ab()
-    plot_wins(result, 'Tic Tac Toe - MinMax vs AlphaBeta')
-    plot_lines(result, 1800, 'Tic Tac Toe - AlphaBeta vs MinMax - Execution iterations')
+    # solver.display_tic_tac_toe(X=solver.human_player, O=solver.alpha_beta_player, game_width=3)
+    # calculate = solver.display_tic_tac_toe(X=solver.minimax_player, O=solver.alpha_beta_player, game_width=3)
+    calculate = mm_vs_ab()
+    plot_wins(calculate, 'Tic Tac Toe - MinMax vs AlphaBeta')
+    plot_lines(calculate, 1800, 'Tic Tac Toe - AlphaBeta vs MinMax - Execution iterations')
 
-    result = ab_vs_mm()
-    plot_wins(result, 'Tic Tac Toe - AlphaBeta vs MinMax')
-    plot_lines(result, 1800, 'Tic Tac Toe - AlphaBeta vs MinMax - Execution iterations')
+    calculate = ab_vs_mm()
+    plot_wins(calculate, 'Tic Tac Toe - AlphaBeta vs MinMax')
+    plot_lines(calculate, 1800, 'Tic Tac Toe - AlphaBeta vs MinMax - Execution iterations')
 
     # raw_input()
